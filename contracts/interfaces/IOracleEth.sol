5fbffc0cb7c4f23efaf0b3457f8cd5261fb3872b
34x4p08 <34x4p08@gmail.com>
2021-04-07 17:45:21 +0300

    Describe return values
    Signed-off-by: 34x4p08 <34x4p08@gmail.com>

--- contracts/interfaces/IOracleEth.sol
+++ contracts/interfaces/IOracleEth.sol
@@ -1,6 +1,7 @@
 interface IOracleEth {
 
     // returns Q112-encoded value
+    // returned value 10**18 * 2**112 is 1 Ether
     function assetToEth(address asset, uint amount) external view returns (uint);
 
     // returns the value "as is"
