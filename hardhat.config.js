02087eb270dccb66f96fd76584f92e15934e31d4
Eenae <z-line@my.com>
2021-09-28 16:40:21 +0300

    core deployment rewritten as a task

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,29 @@ require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-waffle");
 
 
+task('deploy', 'Runs a core deployment')
+    .addParam('foundation', 'Address of a foundation account/contract')
+    .addParam('manager', 'Address of a manager account/contract')
+    .addParam('wtoken', 'Address of a wrapped network token (e.g. WETH for Ethereum)')
+    .addOptionalParam('deployer', 'Address of a deployer account to use (defaults to the first account)')
+    .setAction(async (taskArgs) => {
+        const {createDeployment} = require('./lib/deployments/core');
+        const {runDeployment} = require('./test/helpers/deployUtils');
+
+        const deployer = taskArgs.deployer ? taskArgs.deployer : (await ethers.getSigners())[0].address;
+        const deployment = await createDeployment({
+            deployer,
+            foundation: taskArgs.foundation,
+            manager: taskArgs.manager,
+            wtoken: taskArgs.wtoken
+        });
+
+        const deployed = await runDeployment(deployment, {deployer});
+
+        console.log('Success!', deployed);
+    });
+
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
