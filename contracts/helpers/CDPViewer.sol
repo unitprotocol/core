d3a94be60790671ffbd296ed19a1c4af38cf1248
pum <pum@purum>
2022-03-14 16:03:10 +0300

    cdp viewer

---
+++ contracts/helpers/CDPViewer.sol
@@ -0,0 +1,226 @@
+// SPDX-License-Identifier: bsl-1.1
+
+/*
+  Copyright 2021 Unit Protocol: Artem Zakharov (az@unit.xyz).
+*/
+pragma solidity 0.7.6;
+
+pragma experimental ABIEncoderV2;
+
+import "../interfaces/IVault.sol";
+import "../interfaces/IVaultParameters.sol";
+import "../interfaces/IVaultManagerParameters.sol";
+import "../interfaces/IOracleRegistry.sol";
+import "./IUniswapV2PairFull.sol";
+import "./ERC20Like.sol";
+
+
+/**
+ * @notice Views collaterals in one request to save node requests and speed up dapps.
+ */
+contract CDPViewer {
+
+    IVault public immutable vault;
+    IVaultParameters public immutable vaultParameters;
+    IVaultManagerParameters public immutable vaultManagerParameters;
+    IOracleRegistry public immutable oracleRegistry;
+
+    struct CDP {
+
+        // Collateral amount
+        uint128 collateral;
+
+        // Debt amount
+        uint128 debt;
+
+        // Debt + accrued stability fee
+        uint totalDebt;
+
+        // Percentage with 3 decimals
+        uint32 stabilityFee;
+
+        uint32 lastUpdate;
+
+        // Percentage with 0 decimals
+        uint16 liquidationFee;
+
+        uint16 oracleType;
+    }
+
+    struct CollateralParameters {
+
+        // USDP mint limit
+        uint128 tokenDebtLimit;
+
+        // USDP mint limit
+        uint128 tokenDebt;
+
+        // Percentage with 3 decimals
+        uint32 stabilityFee;
+
+        // Percentage with 3 decimals
+        uint32 liquidationDiscount;
+
+        // Devaluation period in blocks
+        uint32 devaluationPeriod;
+
+        // Percentage with 0 decimals
+        uint16 liquidationRatio;
+
+        // Percentage with 0 decimals
+        uint16 initialCollateralRatio;
+
+        // Percentage with 0 decimals
+        uint16 liquidationFee;
+
+        // Oracle types enabled for this asset
+        uint16 oracleType;
+
+        // Percentage with 2 decimals (basis points)
+        uint16 borrowFee;
+
+        CDP cdp;
+    }
+
+    struct TokenDetails {
+        address[2] lpUnderlyings;
+        uint128 balance;
+        uint128 totalSupply;
+        uint8 decimals;
+        address uniswapV2Factory;
+
+        address underlyingToken;
+        uint256 underlyingTokenBalance;
+        uint256 underlyingTokenTotalSupply;
+        uint8 underlyingTokenDecimals;
+        address underlyingTokenUniswapV2Factory;
+        address[2] underlyingTokenUnderlyings;
+
+    }
+
+
+    constructor(address _vaultManagerParameters, address _oracleRegistry) {
+         IVaultManagerParameters vmp = IVaultManagerParameters(_vaultManagerParameters);
+         vaultManagerParameters = vmp;
+         IVaultParameters vp = IVaultParameters(vmp.vaultParameters());
+         vaultParameters = vp;
+         vault = IVault(vp.vault());
+         oracleRegistry = IOracleRegistry(_oracleRegistry);
+    }
+
+    /**
+     * @notice Get parameters of one asset
+     * @param asset asset address
+     * @param owner owner address
+     */
+    function getCollateralParameters(address asset, address owner)
+        public
+        view
+        returns (CollateralParameters memory r)
+    {
+        r.stabilityFee = uint32(vaultParameters.stabilityFee(asset));
+        r.liquidationFee = uint16(vaultParameters.liquidationFee(asset));
+        r.initialCollateralRatio = uint16(vaultManagerParameters.initialCollateralRatio(asset));
+        r.liquidationRatio = uint16(vaultManagerParameters.liquidationRatio(asset));
+        r.liquidationDiscount = uint32(vaultManagerParameters.liquidationDiscount(asset));
+        r.devaluationPeriod = uint32(vaultManagerParameters.devaluationPeriod(asset));
+
+        r.tokenDebtLimit = uint128(vaultParameters.tokenDebtLimit(asset));
+        r.tokenDebt = uint128(vault.tokenDebts(asset));
+        r.oracleType = uint16(oracleRegistry.oracleTypeByAsset(asset));
+
+        r.borrowFee = 0;
+
+        if (owner == address(0)) return r;
+        r.cdp.stabilityFee = uint32(vault.stabilityFee(asset, owner));
+        r.cdp.liquidationFee = uint16(vault.liquidationFee(asset, owner));
+        r.cdp.debt = uint128(vault.debts(asset, owner));
+        r.cdp.totalDebt = vault.getTotalDebt(asset, owner);
+        r.cdp.collateral = uint128(vault.collaterals(asset, owner));
+        r.cdp.lastUpdate = uint32(vault.lastUpdate(asset, owner));
+        r.cdp.oracleType = uint16(vault.oracleType(asset, owner));
+    }
+
+    /**
+     * @notice Get details of one token
+     * @param asset token address
+     * @param owner owner address
+     */
+    function getTokenDetails(address asset, address owner)
+        public
+        view
+        returns (TokenDetails memory r)
+    {
+        address token0;
+        address token1;
+
+        (bool success, bytes memory data) = asset.staticcall{gas:20000}(abi.encodeWithSignature("token0()"));
+        if (success && data.length == 32) { // check in this way (and not try/catch) since some tokens has fallback functions
+            token0 = bytesToAddress(data);
+
+            (success, data) = asset.staticcall{gas:20000}(abi.encodeWithSignature("token1()"));
+            if (success && data.length == 32) {
+                token1 = bytesToAddress(data);
+
+                (success, data) = asset.staticcall{gas:20000}(abi.encodeWithSignature("factory()"));
+                if (success && data.length == 32) {
+                    r.lpUnderlyings[0] = token0;
+                    r.lpUnderlyings[1] = token1;
+                    r.uniswapV2Factory = bytesToAddress(data);
+                }
+            }
+        }
+
+        r.totalSupply = uint128(IUniswapV2PairFull(asset).totalSupply());
+        r.decimals = uint8(IUniswapV2PairFull(asset).decimals());
+        if (owner != address(0)) {
+            r.balance = uint128(ERC20Like(asset).balanceOf(owner));
+        }
+    }
+
+    function bytesToAddress(bytes memory _bytes) private pure returns (address addr) {
+        assembly {
+          addr := mload(add(_bytes, 32))
+        }
+    }
+
+    function bytesToBytes32(bytes memory _bytes) private pure returns (bytes32 _bytes32) {
+        assembly {
+          _bytes32 := mload(add(_bytes, 32))
+        }
+    }
+
+    /**
+     * @notice Get parameters of many collaterals
+     * @param assets asset addresses
+     * @param owner owner address
+     */
+    function getMultiCollateralParameters(address[] calldata assets, address owner)
+        external
+        view
+        returns (CollateralParameters[] memory r)
+    {
+        uint length = assets.length;
+        r = new CollateralParameters[](length);
+        for (uint i = 0; i < length; ++i) {
+            r[i] = getCollateralParameters(assets[i], owner);
+        }
+    }
+
+    /**
+     * @notice Get details of many token
+     * @param assets token addresses
+     * @param owner owner address
+     */
+    function getMultiTokenDetails(address[] calldata assets, address owner)
+        external
+        view
+        returns (TokenDetails[] memory r)
+    {
+        uint length = assets.length;
+        r = new TokenDetails[](length);
+        for (uint i = 0; i < length; ++i) {
+            r[i] = getTokenDetails(assets[i], owner);
+        }
+    }
+}
